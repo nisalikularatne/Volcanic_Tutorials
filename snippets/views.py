e20fd1c16fea986fb82e8e5a589699243b50e9e2
Nisali Kularatne <nisalikularatne@yahoo.com>
2018-06-05 10:53:30 +0800

    Initial commit

---
+++ snippets/views.py
@@ -0,0 +1,13 @@
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
+from rest_framework.generics import ListCreateAPIView,RetrieveUpdateDestroyAPIView
+
+
+class SnippetList(ListCreateAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+
+class SnippetDetail(RetrieveUpdateDestroyAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
